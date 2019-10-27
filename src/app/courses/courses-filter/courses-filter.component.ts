85afb2837e48d50876a1352a3c90befb417ccce0
Nitin Jaryal <nitin_jaryal@epam.com>
2019-10-28 03:53:18 +0530

    Updates as per comments

--- src/app/courses/courses-filter/courses-filter.component.ts
+++ src/app/courses/courses-filter/courses-filter.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-filter.component.scss']
 })
 export class CoursesFilterComponent implements OnInit {
-  searchText: any;
+  searchText: string;
   constructor() { }
 
   ngOnInit() {
